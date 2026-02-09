002929149413e387e24fe35fb2fbcaf92e9051f3
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2026-02-08 18:03:27 -0600

    Refactor/reformat try.sh

--- try.sh
+++ try.sh
@@ -5,8 +5,9 @@ if set -q argv[1]
 	set generation $argv[1]
 end
 
+if true
 # Train with this permutation of 1/l training data
-uv.exe run cluster.py ../EFTA00382108-002.png ./train_top.txt ./train_bot.txt -q
+uv.exe run cluster.py ../EFTA00382108-002.png ./train_top.txt ./train_bot.txt -q -d
 
 # Generate outputs according to what we just trained
 parallel -j 16 uv.exe run cluster.py ../EFTA00382108-{}.png -o EFTA00382108-{}.txt ::: (printf "%03d\n" (seq 2 23))
@@ -16,9 +17,11 @@ tail -n +7 < EFTA00382108-002.txt | rewrite EFTA00382108-002.txt
 
 # Remove everything after the single = padding on page 23
 string replace -r "(?s)=.*" "=" "$(cat EFTA00382108-023.txt)" | rewrite EFTA00382108-023.txt
+end
 
 # Concatenate all and try to generate output
-cat EFTA00382108-*.txt | string trim -c "> \r" | b64-debug/target/release/b64-debug --strict > airmail.pdf
+cat EFTA00382108-*.txt | string replace "> " "" \
+	| b64-debug/target/release/b64-debug --strict > airmail.pdf
 
 # Save results and training data to a subfolder
 set DEST EFTA00382108/$generation
