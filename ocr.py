5f6ce6641207f8a2002e341fc9a6e06071f3d5f2
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2026-02-10 22:17:54 -0600

    Improved OCR.py

--- ocr.py
+++ ocr.py
@@ -3,7 +3,7 @@ import argparse
 import cv2
 import numpy as np
 import torch
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 from ultralytics import YOLO
 
 # --- CONFIGURATION ---
@@ -21,10 +21,14 @@ MODEL_NAME = "yolo26n.pt" # Latest YOLO version
 class YOLO_OCR:
     def __init__(self, model_path=None):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        if model_path and os.path.exists(model_path):
+        if model_path:
+            if not os.path.exists(model_path):
+                raise Exception(f"Model {model_path} not found")
             self.model = YOLO(model_path)
+            self.fine_tune = True
         else:
             self.model = YOLO(MODEL_NAME)
+            self.fine_tune = False
 
     # --- PART 1: DATA GENERATION ---
     def generate_data(self, count=1000, split='train'):
@@ -41,15 +45,30 @@ class YOLO_OCR:
             return
 
         for i in range(count):
-            # 1. Create Canvas
+            # Create base canvas
             img = Image.new('RGB', (CANVAS_W, CANVAS_H), (255, 255, 255))
             draw = ImageDraw.Draw(img)
 
-            # 2. Generate Random Text
-            text_len = random.randint(10, 80)
-            text = "".join(random.choices(ALPHABET, k=text_len))
+            # Generate random text, oversampling tricky characters
+            hard_chars = "ijlI1t/f"
+            # text_len = random.randint(10, 80)
+            # random.triangular(low, high, mode)
+            text_len = int(random.triangular(10, 85, 72))
 
-            # 3. Calculate text dimensions for centering
+            # 40% of the time, fill at least 30% of the slots with confusable characters
+            if True and random.random() < 0.65:
+                num_hard = int(text_len * 0.3)
+                num_normal = text_len - num_hard
+
+                # Create a mixed pool and shuffle it
+                pool = random.choices(hard_chars, k=num_hard) + random.choices(ALPHABET, k=num_normal)
+                random.shuffle(pool)
+                text = "".join(pool)
+            else:
+                # Standard random distribution
+                text = "".join(random.choices(ALPHABET, k=text_len))
+
+            # Calculate text dimensions for centering
             total_w = font.getlength(text)
             curr_x = (CANVAS_W - total_w) // 2
             curr_y = (CANVAS_H - FONT_SIZE) // 2
@@ -60,6 +79,9 @@ class YOLO_OCR:
 
             for j, char in enumerate(text):
                 char_w = font.getlength(char)
+                # Use minimum bbox width for thin chars like 1l/i
+                # They seem to default to ~4px
+                char_w = max(char_w, 6)
 
                 # Bounding Box (Normalized)
                 x_center = (curr_x + char_w/2) / CANVAS_W
@@ -71,9 +93,20 @@ class YOLO_OCR:
                 draw.text((curr_x, curr_y), char, font=font, fill=(0,0,0))
                 curr_x += char_w
 
-            # Save
+            # Add some synthetic noise and blur to help with jpeg detection
+            # if self.fine_tune:
+            #     radius = random.uniform(0.0, 0.2)
+            #     img = img.filter(ImageFilter.GaussianBlur(radius))
+
+            # Save (at slightly lower resolution when fine-tuning)
             name = f"{split}_{i:05d}"
-            img.save(os.path.join(img_dir, f"{name}.jpg"))
+            img_path = os.path.join(img_dir, f"{name}.jpg")
+            if not self.fine_tune:
+                # default quality is 75!?
+                img.save(img_path, "JPEG", quality=95)
+            else:
+                img.save(img_path, "JPEG", quality=random.randint(85, 95))
+
             with open(os.path.join(lbl_dir, f"{name}.txt"), "w") as f:
                 f.write("\n".join(labels))
 
@@ -88,23 +121,34 @@ class YOLO_OCR:
             for i, c in IDX_TO_CHAR.items():
                 f.write(f"  {i}: '{c}'\n")
 
-        self.model.train(
-            data=yaml_path,
-            epochs=100,
-            batch=64,
-            imgsz=1280,
-            rect=True,
-            device=self.device,
-            mosaic=0.0,
-            fliplr=0.0,
-            flipud=0.0,
-            mixup=0.0,
-            copy_paste=0.0,
-            degrees=0.0,
-            scale=0.2,
-            perspective=0.0,
-            cache=True,
-        )
+        train_args = {
+            "device": self.device,
+            "data": yaml_path,
+            "epochs": 100,
+            "batch": 48,
+            "imgsz": 1600,
+            "rect": True,
+            "mosaic": 0.0,
+            "close_mosaic": 0,
+            "fliplr": 0.0,
+            "flipud": 0.0,
+            "mixup": 0.0,
+            "copy_paste": 0.0,
+            "degrees": 0.0,
+            "scale": 0.2,
+            "perspective": 0.0,
+            "cache": False,
+            "erasing": 0.0,
+        }
+
+        # Override certain args when fine tuning
+        fine_tune_args = {
+            "epochs": 50, # or 30
+            "warmup_epochs": 0,
+            "pretrained": True,
+        } if self.fine_tune else {}
+
+        self.model.train(**(train_args | fine_tune_args))
 
     # --- PART 3: INFERENCE & DOCUMENT PROCESSING ---
     def process_document(self, image_path):
@@ -154,18 +198,19 @@ class YOLO_OCR:
             offset_x = (CANVAS_W - w) // 2
             offset_y = (CANVAS_H - h) // 2
             canvas[offset_y:offset_y+h, offset_x:offset_x+w] = line_img
+            canvas_bgr = cv2.cvtColor(canvas, cv2.COLOR_GRAY2BGR)
 
             # Debug
-            cv2.imwrite("debug_canvas.jpg", canvas)
+            cv2.imwrite(f"debug_canvas-{y1}.png", canvas)
 
             # Predict
-            results = self.model.predict(canvas, imgsz=1280, conf=0.25, verbose=False)
+            results = self.model.predict(canvas_bgr, imgsz=1600, conf=0.10, verbose=False)
 
             # This creates a BGR image with boxes and labels drawn on it
             annotated_frame = results[0].plot()
 
             # Save or display it
-            cv2.imwrite("detected_line.jpg", annotated_frame)
+            cv2.imwrite(f"detected_line.jpg", annotated_frame)
 
             # Extract and sort by X-coordinate
             boxes = []
@@ -188,10 +233,13 @@ if __name__ == "__main__":
     parser.add_argument("--train", action="store_true", help="Train the model")
     parser.add_argument("--predict", type=str, help="Path to image for inference")
     parser.add_argument("--model", type=str, default="runs/detect/train/weights/best.pt", help="Path to weights")
+    parser.add_argument("--resume", type=str, help="Training run number to resume from")
     args = parser.parse_args()
 
     # Determine which model to load
     m_path = args.model if os.path.exists(args.model) else None
+    if m_path is None and args.resume is not None:
+        m_path = f"runs/detect/train{args.resume}/weights/best.pt"
     ocr = YOLO_OCR(m_path)
 
     if args.generate:
